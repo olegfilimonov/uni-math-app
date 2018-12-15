ff270c7fcb6a2493b626d0e949a912e278ab4716
Oleg Filimonov <themraction@gmail.com>
2018-12-16 00:07:29 +0300

    add ui and input logic

--- eltech-math-app/Form1.Designer.cs
+++ eltech-math-app/Form1.Designer.cs
@@ -28,20 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьТаблицуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видФункцииToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.своднаяТаблицаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.labelN = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.видФункцииToolStripMenuItem,
+            this.своднаяТаблицаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1105, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьТаблицуToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // загрузитьТаблицуToolStripMenuItem
+            // 
+            this.загрузитьТаблицуToolStripMenuItem.Name = "загрузитьТаблицуToolStripMenuItem";
+            this.загрузитьТаблицуToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьТаблицуToolStripMenuItem.Text = "Загрузить таблицу";
+            this.загрузитьТаблицуToolStripMenuItem.Click += new System.EventHandler(this.загрузитьТаблицуToolStripMenuItem_Click);
+            // 
+            // видФункцииToolStripMenuItem
+            // 
+            this.видФункцииToolStripMenuItem.Name = "видФункцииToolStripMenuItem";
+            this.видФункцииToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.видФункцииToolStripMenuItem.Text = "Вид Функции";
+            // 
+            // своднаяТаблицаToolStripMenuItem
+            // 
+            this.своднаяТаблицаToolStripMenuItem.Name = "своднаяТаблицаToolStripMenuItem";
+            this.своднаяТаблицаToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+            this.своднаяТаблицаToolStripMenuItem.Text = "Сводная таблица";
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(31, 38);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(66, 20);
+            this.textBoxN.TabIndex = 1;
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(12, 41);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(13, 13);
+            this.labelN.TabIndex = 2;
+            this.labelN.Text = "n";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(31, 64);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(66, 20);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "α";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(132, 38);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(229, 20);
+            this.textBoxX.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(132, 64);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(229, 20);
+            this.textBoxY.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(114, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "y";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 105);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(349, 367);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 490);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(349, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Построить таблицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(367, 38);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(726, 518);
+            this.cartesianChart1.TabIndex = 5;
+            this.cartesianChart1.Text = "cartesianChart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1057, 761);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1105, 568);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Построение регрессивных моделей";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem видФункцииToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem своднаяТаблицаToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьТаблицуToolStripMenuItem;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
     }
 }
 
--- eltech-math-app/Form1.cs
+++ eltech-math-app/Form1.cs
@@ -1,20 +1,137 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace eltech_math_app
 {
     public partial class Form1 : Form
     {
+        DataTable dt = new DataTable();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var validForm = validateInput();
+
+            if (!validForm) return;
+
+            var a = double.Parse(textBoxA.Text);
+            var n = double.Parse(textBoxN.Text);
+        }
+
+        private bool validateInput()
+        {
+            var errors = new ArrayList();
+            double doubleTemp;
+
+            if (string.IsNullOrWhiteSpace(textBoxN.Text)) errors.Add("Поле \"n\" пустое или содержит только пробелы");
+            else if (!double.TryParse(textBoxN.Text, out doubleTemp)) errors.Add("Поле \"n\" должно быть числом");
+
+            if (string.IsNullOrWhiteSpace(textBoxA.Text)) errors.Add("Поле \"a\" пустое или содержит только пробелы");
+            else if (!double.TryParse(textBoxA.Text, out doubleTemp)) errors.Add("Поле \"a\" должно быть числом");
+
+            var valid = errors.Count == 0;
+
+            if (!valid)
+            {
+                var message = string.Join("\n", errors.ToArray());
+                MessageBox.Show(message, "Ошибка проверки исходных данных");
+            }
+
+            return errors.Count == 0;
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            if (dt.Columns.Count == 0)
+            {
+                getTableDataSafe();
+            }
+        }
+
+        private void getTableDataSafe()
+        {
+            try
+            {
+                getTableData();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Ошибка загрузки файла");
+            }
+        }
+
+        private void getTableData()
+        {
+            dt = new DataTable();
+            var d = new OpenFileDialog();
+            d.ShowDialog();
+
+            var file = new System.IO.StreamReader(d.FileName);
+            var columns = file.ReadLine().Split(',');
+            foreach (var c in columns)
+            {
+                dt.Columns.Add(c);
+            }
+
+            string newline;
+            while ((newline = file.ReadLine()) != null)
+            {
+                var dr = dt.NewRow();
+                var values = newline.Split(',');
+                for (var i = 0; i < values.Length; i++)
+                {
+                    dr[i] = values[i];
+                }
+
+                dt.Rows.Add(dr);
+            }
+
+            file.Close();
+            dataGridView1.DataSource = dt;
+            foreach (DataGridViewTextBoxColumn column in dataGridView1.Columns)
+            {
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                column.DefaultCellStyle.Padding = new Padding(10, 0, 10, 0);
+            }
+
+            List<double> xValues = dt.AsEnumerable().Select(x => Convert.ToDouble(x["x"])).ToList();
+            List<double> yValues = dt.AsEnumerable().Select(x => Convert.ToDouble(x["y"])).ToList();
+
+            ChartValues<double> chartValues = new ChartValues<double>(yValues);
+
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Число травмированных",
+                    Values = chartValues,
+                }
+            };
+
+            cartesianChart1.AxisX[0].Title = "Кол-во пожаров";
+            cartesianChart1.AxisX[0].Labels = xValues.ConvertAll(x => x.ToString());
+            cartesianChart1.AxisY[0].Title = "Число травмированных";
+
+            cartesianChart1.LegendLocation = LegendLocation.None;
+        }
+
+        private void загрузитьТаблицуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            getTableDataSafe();
         }
     }
 }
